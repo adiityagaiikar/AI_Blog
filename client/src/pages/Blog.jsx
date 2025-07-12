f05459fd1d736fa076193d663a3782163ce1efee
adiityagaiikar <adiityagaiikar@gmail.com>
2025-07-12 13:00:49 +0530

    Initial commit

---
+++ client/src/pages/Blog.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import {useParams} from 'react-router-dom'
+import {assets, blog_data} from '../assets/assets'
+import Navbar from '../components/Navbar'
+
+const Blog = () => {
+
+  const {id} =useParams()
+
+  const [data,setData]=useState(null)
+
+  const fetchBlogData=async()=>{
+    const data = blog_data.find(item => item._id === id)
+    setData(data)
+  }
+
+  useEffect(()=>{
+    fetchBlogData()
+  },[])
+
+  return data ? (
+    <div className='relative'>
+      <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-1 opacity-50' />
+      
+      <Navbar />
+      <div></div>
+
+      <div></div>
+
+    </div>
+  ) : <div>Loading</div>
+}
+
+export default Blog
